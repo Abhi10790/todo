44eacdfdb1ce8f377e4764f75eec3fefcc20824b
Abhijeet <abhijeetukalbhor@gmail.com>
2021-06-22 19:32:35 +0530

    test

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -17,7 +17,6 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR=os.path.join(BASE_DIR,'templates')
 STATIC_DIR=os.path.join(BASE_DIR,'static')
-heroku config:set DISABLE_COLLECTSTATIC=1
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
